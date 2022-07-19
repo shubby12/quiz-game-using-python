d42544d43d02a25c19a5ccd95074b8c0f489b21c
Shubham Yadav <55327842+shubby12@users.noreply.github.com>
2022-07-20 00:10:11 +0530

    code shifted

---
+++ quiz-game.py
@@ -0,0 +1,139 @@
+
+
+print("Welcome to the quiz!!")
+
+playing = input("Do you want to play this game? ")
+
+if playing != "yes":
+    quit()
+
+score = 0
+print("Okay, Let's start the game then :) ")
+
+answer = input("121 Divided by 11 is: ")
+if answer == "11":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("60 Times of 8 Equals to: ")
+if answer == "480":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("What is the Next Prime Number after 7: ")
+if answer == "11":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("The Product of 131 * 0 * 300 * 4: ")
+if answer == "0":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("Solve 3 + 6 * ( 5 + 4) ÷ 3 - 7: ")
+if answer == "14":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("Solve 23 + 3 ÷ 3: ")
+if answer == "24":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("What is 6% Equals to: ")
+if answer == "0.06":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("How Many Years are there in a Decade? ")
+if answer == "10":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("How Many Months Make a Century? ")
+if answer == "1200":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Score is : ", score)
+    print("Let's move to next question")
+else :
+    print(" Incorrect Answer")
+    print("Your Score is : ", score)
+    print("The next question is: ")
+
+
+
+answer = input("Add the Decimals 5.23 + 8.79: ")
+if answer == "14.02":
+    score = score+1
+    print("You're correct :) ")
+    print("Your Final Score is : ", score)
+    print("The game is over. Thankyou for playing the game.")
+else :
+    print(" Incorrect Answer")
+    print("Your Final Score is : ", score)
+    print("The game is over. Thankyou for playing the game.")
